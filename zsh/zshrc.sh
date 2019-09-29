01c8b785e07af0518d82159b92d87bdb605b68fb
Emassei <Emassei@users.noreply.github.com>
2019-09-29 09:05:59 -0700

    add this ranger alias to exit ranger into the cwd

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -9,7 +9,7 @@
 # Aliases
 	alias v="vim -p"
 	mkdir -p /tmp/log
-	
+	alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 	# This is currently causing problems (fails when you run it anywhere that isn't a git project's root directory)
 	# alias vs="v `git status --porcelain | sed -ne 's/^ M //p'`"
 
