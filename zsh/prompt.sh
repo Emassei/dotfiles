8dafec64a05fa21f64ac09005fad4957920c04b9
Emassei <Emassei@users.noreply.github.com>
2020-02-15 15:19:30 -0500

    Get rid of the sudo line from the prompt.

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -38,12 +38,6 @@ set_prompt() {
 	fi
 
 	# Sudo: https://superuser.com/questions/195781/sudo-is-there-a-command-to-check-if-i-have-sudo-and-or-how-much-time-is-left
-	CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1|grep "load"|wc -l)
-	if [ ${CAN_I_RUN_SUDO} -gt 0 ]
-	then
-		PS1+=', '
-		PS1+="%{$fg_bold[red]%}SUDO%{$reset_color%}"
-	fi
 
 	PS1+="%{$fg[white]%}]: %{$reset_color%}% "
 }
