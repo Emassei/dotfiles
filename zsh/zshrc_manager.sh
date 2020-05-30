6b1b43bb91c20a93bc57f4c4b91da91338f37048
Emassei <Emassei@users.noreply.github.com>
2020-05-29 21:01:54 -0500

    add readme stuff and dont update every time you start a shell good god

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -2,11 +2,11 @@ time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
 if command -v tmux>/dev/null; then
 	[ -z $TMUX ] && exec tmux
-else 
+else
 	echo "tmux not installed. Run ./deploy to configure dependencies"
 fi
 
-echo "Updating configuration"
+echo "What up Nerd?"
 #(cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
-(cd ~/dotfiles && git pull && git submodule update --init --recursive)
+#(cd ~/dotfiles && git pull && git submodule update --init --recursive)
 source ~/dotfiles/zsh/zshrc.sh
