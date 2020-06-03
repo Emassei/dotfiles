a1ca4832781cd9136074807efaaa44cfbae3f13c
Ernie <Emassei@users.noreply.github.com>
2020-06-03 11:59:54 -0500

    Add luke smiths, credit to him, shell scripts to my dot files

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,10 +1,10 @@
 time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
-if command -v tmux>/dev/null; then
-	[ -z $TMUX ] && exec tmux
-else
-	echo "tmux not installed. Run ./deploy to configure dependencies"
-fi
+#if command -v tmux>/dev/null; then
+	#[ -z $TMUX ] && exec tmux
+#else
+	#echo "tmux not installed. Run ./deploy to configure dependencies"
+#fi
 
 echo "What up Nerd?"
 #(cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
