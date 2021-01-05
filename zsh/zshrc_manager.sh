e59e509caa070d1d8bf1085e30affcea90d5a466
Ernie <Emassei@users.noreply.github.com>
2021-01-05 14:49:54 -0500

    update display script and shell welcome text

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -6,7 +6,7 @@ time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 	#echo "tmux not installed. Run ./deploy to configure dependencies"
 #fi
 
-echo "What up Nerd?"
+echo "What up Nerd? Write in your stupid journal"
 #(cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
 #(cd ~/dotfiles && git pull && git submodule update --init --recursive)
 source ~/dotfiles/zsh/zshrc.sh
