cb2be8f3cda559e1005c5e888c252966a97fb680
Ernie Massei <Emassei@users.noreply.github.com>
2021-05-29 22:31:55 -0500

    add vpn scan

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -4,7 +4,7 @@ time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
     ## if not inside a tmux session, and if no session is started, start a new session
     #[ -z "${tmux}" ] && (tmux attach >/dev/null 2>&1 || tmux)
 #fi
-echo "es dificil recorder las reglas\nyo termino de tabajar\nnecesitas dejar de fumar\nyo entiendo mas de lo que hablo\nen colombia hay mas de 50 millones de personas"
+vpn_scan
 #echo "What up Nerd? Write in your stupid journal!"
 #(cd ~/dotfiles && time_out 3 git pull && time_out 3 git submodule update --init --recursive)
 #(cd ~/dotfiles && git pull && git submodule update --init --recursive)
